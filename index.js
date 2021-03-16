d477ea187219436e819c883952fe45258db95150
giangthang <shikaku04021992@gmail.com>
2021-03-16 16:54:41 +0700

    done

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const ejsengine = require('ejs-mate');
 const port = 3000;
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views','./views');
+app.use('/public',express.static('./public'));
+app.engine('ejs',ejsengine);
+
+
 app.get('/', (req, res) => {
-    res.send('Dung Giang Dung de');
+    res.render('page/index');
+});
+app.get('/about', (req, res) => {
+    res.render('page/about');
 });
 app.listen(port, ()=>{
     console.log('> Running');;
